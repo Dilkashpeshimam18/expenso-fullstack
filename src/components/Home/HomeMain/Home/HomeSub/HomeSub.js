3323b18dabeab4c5ae15692370a1a4168d7c3dd7
Dilkash <dilkashpeshimam@gmail.com>
2022-12-16 12:07:20 +0530

    showing total expense & remaining amount

--- src/components/Home/HomeMain/Home/HomeSub/HomeSub.js
+++ src/components/Home/HomeMain/Home/HomeSub/HomeSub.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './HomeSub.css'
-const HomeSub = () => {
+import AddIcon from '../../../../assets/plus.png'
+const HomeSub = ({ title, remaining, amount, handleClickOpen }) => {
     return (
-        <div className='homeSub'>HomeSub</div>
+        <div className='homeSub'>
+            <div className='homeSub__container1'>
+                <p style={{ marginLeft: '5px' }}>{title}</p>
+                {!remaining && <img style={{ height: '20px', width: '20px', cursor: 'pointer', marginRight: '5px', marginTop: '12px' }} onClick={handleClickOpen} src={AddIcon} />}
+
+            </div>
+            <div className='homeSub__container2'>
+                <p style={{ fontWeight: 'bold', margin: '0', paddingTop: '5px', color: 'rgb(1, 140, 140)' }}>Rs {amount}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Home/HomeMain/HomeMain.js
+++ src/components/Home/HomeMain/HomeMain.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HomeCategory from './Home/HomeCategory/HomeCategory'
 import HomeGraph from './Home/HomeGraph/HomeGraph'
 import HomeSub from './Home/HomeSub/HomeSub'
+import IncomeModal from './Home/IncomeModal/IncomeModal'
+import { useSelector } from 'react-redux'
 import './HomeMain.css'
+
 const HomeMain = () => {
+    const [remaining, setRemaining] = useState(true)
+    const [open, setOpen] = useState(false);
+    const [income, setIncome] = useState(() => {
+        return localStorage.getItem('userIncome') || 0
+    })
+    const [userIncome, setUserIncome] = useState(() => {
+        return localStorage.getItem('userIncome')
+    })
+    const allExpenses = useSelector(state => state.expenses.expenses)
+    let totalExpense = allExpenses.reduce((curr, expense) => {
+        return curr + Number(expense.amount)
+    }, 0)
+    let remainingAmount = income - totalExpense
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleIncome = () => {
+
+        localStorage.setItem('userIncome', income)
+        let Income = localStorage.getItem('userIncome')
+        setUserIncome(Income)
+    }
+    const handleChange = (e) => {
+        setIncome(e.target.value)
+    }
+
     return (
         <div className='homeMain'>
             <div className='home__subContainer'>
-                <HomeSub />
-                <HomeSub />
-                <HomeSub />
+                <HomeSub title='Income' amount={userIncome} handleClickOpen={handleClickOpen} />
+                <HomeSub title='Expense' remaining={remaining} amount={totalExpense} />
+                <HomeSub title='Remaining' remaining={remaining} amount={remainingAmount} />
             </div>
             <div className='home__graphContainer'>
                 <HomeGraph />
@@ -23,6 +57,7 @@ const HomeMain = () => {
 
 
             </div>
+            <IncomeModal handleClose={handleClose} open={open} handleIncome={handleIncome} handleChange={handleChange} />
         </div>
     )
 }
