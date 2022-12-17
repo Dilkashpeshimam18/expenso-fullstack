c411e84824ba080685e7ee5f97b555e2deeed1ca
Dilkash <dilkashpeshimam@gmail.com>
2022-12-17 17:16:34 +0530

    showing user detail in dashboard

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,7 @@ import VerifyEmail from '../VerifyEmail/VerifyEmail'
 import { useSelector, useDispatch } from 'react-redux'
 import { themeActions } from '../../store/slice/theme-slice'
 import ReactSwitch from 'react-switch'
-import Avatar from '@mui/material/Avatar';
 import { Link } from 'react-router-dom'
-import axios from 'axios'
 const Header = ({ open, setOpen, isUpdated }) => {
     const userToken = useSelector(state => state.auth.userToken)
     const dispatch = useDispatch()
@@ -17,34 +15,7 @@ const Header = ({ open, setOpen, isUpdated }) => {
         console.log(theme)
     }
     const theme = useSelector(state => state.theme.theme)
-    const [name, setName] = useState('')
-    const [photoUrl, setPhotoUrl] = useState('')
 
-    const getUserProfileData = async () => {
-        try {
-            if (userToken) {
-                let data = {
-                    idToken: userToken
-                }
-                const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDKj1Lc9A0JYGLuOTbYEr8SD-7ChLkI1Ys', data, {
-                    headers: {
-                        'Content-Type': 'application/json',
-
-                    }
-                })
-                console.log(response)
-                setName(response.data.users[0].displayName)
-                setPhotoUrl(response.data.users[0].photoUrl)
-            }
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
-    useEffect(() => {
-        getUserProfileData()
-
-    }, [])
     return (
         <div className='header'>
             <div className='header__container'>
@@ -56,6 +27,10 @@ const Header = ({ open, setOpen, isUpdated }) => {
                         <ReactSwitch onChange={handleToggle} checked={theme == 'dark'} />
 
                     </div>
+                    {!userToken && <div className='header__rightSubLink'>
+                        <Link to='/login'><h3>Login</h3></Link>
+
+                    </div>}
 
                     {userToken && <>
                         {open == true && <ProfileModal open={open} setOpen={setOpen} />}
@@ -68,14 +43,6 @@ const Header = ({ open, setOpen, isUpdated }) => {
                             <Logout />
 
                         </div>
-                        <Link style={{ textDecoration: 'none', color: 'black' }} to='/update-profile'>
-                            <div className='header__rightSubLink'>
-                                <Avatar src={photoUrl} />
-
-                                {name != '' ? <p style={{ marginLeft: '15px' }}>Hi, {name}</p> : <p style={{ marginLeft: '15px' }}>Hi, Guest</p>}
-
-                            </div>
-                        </Link>
 
                     </>
 
--- src/components/Home/HomeLeft/HomeLeft.js
+++ src/components/Home/HomeLeft/HomeLeft.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './HomeLeft.css'
 import HomeOption from './HomeOption/HomeOption'
+import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
 const HomeLeft = () => {
+    const [name, setName] = useState('')
+    const [photoUrl, setPhotoUrl] = useState('')
+    const userToken = useSelector(state => state.auth.userToken)
+    const dispatch = useDispatch()
+    const getUserProfileData = async () => {
+        try {
+            if (userToken) {
+                let data = {
+                    idToken: userToken
+                }
+                const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDKj1Lc9A0JYGLuOTbYEr8SD-7ChLkI1Ys', data, {
+                    headers: {
+                        'Content-Type': 'application/json',
+
+                    }
+                })
+                console.log(response)
+                setName(response.data.users[0].displayName)
+                setPhotoUrl(response.data.users[0].photoUrl)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getUserProfileData()
+
+    }, [])
     return (
         <div className='homeLeft'>
+            <div className='homeLeft__userProfile'>
+                <Link style={{ textDecoration: 'none', color: 'black' }} to='/update-profile'>
+                    <div >
+                        <Avatar sx={{ width: 100, height: 105, marginLeft: '25px' }}
+                            src={photoUrl} />
+                        <div className='userProfile__name'>
+                            {name != '' ? <h3 > {name}</h3> : <h3 style={{ marginLeft: '35px' }}>Hi, Guest</h3>}
+
+                        </div>
+
+                    </div>
+                </Link>
+            </div>
             <HomeOption /></div>
     )
 }
