ef611dba0de99dd860d3ca49b2842c60662e42d2
Dilkash <dilkashpeshimam@gmail.com>
2022-12-18 14:25:27 +0530

    added dashboardslice in redux

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,15 @@ import authReducer from './slice/auth-slice'
 import expenseReducer from './slice/expense-slice'
 import themeReducer from "./slice/theme-slice";
 import modalReducer from './slice/modal-slice'
+import dashboardReducer from './slice/dashboard-slice'
+
 const store = configureStore({
     reducer: {
         auth: authReducer,
         expenses: expenseReducer,
         theme: themeReducer,
-        modal: modalReducer
+        modal: modalReducer,
+        dashboard: dashboardReducer
     }
 })
 
