a21c2306ff10b44ba22a985d1a6aede8607b6309
Dilkash <dilkashpeshimam@gmail.com>
2022-12-18 17:07:27 +0530

    changing font color onSelect conditionally

--- src/components/Home/HomeLeft/HomeOption/HomeOption.js
+++ src/components/Home/HomeLeft/HomeOption/HomeOption.js
@@ -6,18 +6,20 @@ import EditIcon from '@mui/icons-material/Edit';
 import ShowChartIcon from '@mui/icons-material/ShowChart';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { dashboardActions } from '../../../../store/slice/dashboard-slice';
 const HomeOption = () => {
     const dispatch = useDispatch()
+    const isSelected = useSelector(state => state.dashboard.isSelected)
+
     return (
         <div className='homeOption'>
             <ul className='homeOption__container' style={{ listStyle: 'none' }}>
-                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Dashboard'))} className='homeOption__list'><DashboardIcon className='homeOption__icon' /><p>Dashboard</p> </li>
+                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Dashboard'))} className={isSelected != 'Dashboard' ? 'homeOption__list' : 'homeOption__listSelected'}><DashboardIcon className='homeOption__icon' /><p>Dashboard</p> </li>
                 <Link style={{ textDecoration: 'none' }} to='update-profile'><li className='homeOption__list'><EditIcon className='homeOption__icon' /><p>Edit Profile</p></li></Link>
-                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Bar'))} className='homeOption__list'><BarChartIcon className='homeOption__icon' /><p>Bar Chart</p></li>
-                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Line'))} className='homeOption__list'><ShowChartIcon className='homeOption__icon' /><p>Line Chart</p></li>
-                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Pie'))} className='homeOption__list'><DonutLargeIcon className='homeOption__icon' /><p>Pie Chart</p></li>
+                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Bar'))} className={isSelected != 'Bar' ? 'homeOption__list' : 'homeOption__listSelected'}><BarChartIcon className='homeOption__icon' /><p>Bar Chart</p></li>
+                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Line'))} className={isSelected != 'Line' ? 'homeOption__list' : 'homeOption__listSelected'}><ShowChartIcon className='homeOption__icon' /><p>Line Chart</p></li>
+                <li onClick={() => dispatch(dashboardActions.handleIsSelected('Pie'))} className={isSelected != 'Pie' ? 'homeOption__list' : 'homeOption__listSelected'}><DonutLargeIcon className='homeOption__icon' /><p>Pie Chart</p></li>
 
             </ul>
         </div >
