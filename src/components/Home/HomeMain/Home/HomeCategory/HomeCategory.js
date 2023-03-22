07c375b1b2660fd147fd1a2519208270187931da
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-03-22 06:08:32 -0700

    separeted client & server

--- src/components/Home/HomeMain/Home/HomeCategory/HomeCategory.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './HomeCategory.css'
-const HomeCategory = () => {
-    return (
-        <div className='homeCategory'>HomeCategory</div>
-    )
-}
-
-export default HomeCategory
