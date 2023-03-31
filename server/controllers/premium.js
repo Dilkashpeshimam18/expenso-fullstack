5cf35c7a6e0608d44995193684bca2f71c30816c
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-03-31 04:09:58 -0700

    optimization leaderboard backend logic by sql join

--- server/controllers/premium.js
+++ server/controllers/premium.js
@@ -1,32 +1,24 @@
 const Expense = require('../models/expense')
 const Users = require('../models/users')
+const sequelize = require('../utils/db')
 
 exports.showLeaderboard = async (req, res) => {
    try {
-      const allExpense = await Expense.findAll()
-      const allUser = await Users.findAll()
-      let map = new Map()
-      let totalExp = new Map()
-      for (let exp of allExpense) {
-         map.set(exp.usersdbId, map.get(exp.usersdbId) + exp.amount || exp.amount)
 
-      }
-      for (let user of allUser) {
-         if (map.has(user.id)) {
-            totalExp.set(user.name, map.get(user.id))
+      const leaderBoardData = await Users.findAll({
+         attributes: ['id', 'name',
+            [sequelize.fn('sum', sequelize.col('amount')), 'total_expense']],
+         include: [
+            {
+               model: Expense,
+               attributes: []
+            }
+         ],
+         group: ['id'],
+         order: [['total_expense', 'DESC']]
 
-         } else {
-            totalExp.set(user.name, 0)
-         }
-
-      }
-      let leaderBoardData = Array.from(totalExp, ([key, value]) => {
-         return {
-            name: key,
-            total_expense: value
-         }
       })
-      leaderBoardData.sort((a, b) => b.total_expense - a.total_expense)
+
 
       res.status(200).json({ succes: true, data: leaderBoardData })
    } catch (err) {
