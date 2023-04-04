cd344fc20e82acd4f35bf8129edd5ea242d5a391
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-04-04 09:16:49 -0700

    created password routes & controller

---
+++ server/routes/password.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const passwordController=require('../controllers/password')
+const router = express.Router()
+
+router.post('/forgotpassword',passwordController.forgotPassword)
+
+module.exports=router;
