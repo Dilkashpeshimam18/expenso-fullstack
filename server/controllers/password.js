02b2e5739bc17ed522855a136123d4c19c4a0aad
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-04-05 00:54:58 -0700

    sending dummy mail using sendinblue to user

--- server/controllers/password.js
+++ server/controllers/password.js
@@ -1,3 +1,37 @@
-exports.forgotPassword=(req,res)=>{
-    
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.SIBEMAIL_APIKEY;
+
+var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+var smtpMailData = new SibApiV3Sdk.SendSmtpEmail();
+
+
+exports.forgotPassword = async (req, res) => {
+    apiKey.apiKey = process.env.SIBEMAIL_APIKEY;
+
+    try {
+        console.log('API_KEY>>>>', apiKey)
+        const email = req.body.email
+        console.log(email)
+
+        const sender = {
+            email: 'dilkashpeshimam@gmail.com'
+        }
+        smtpMailData.sender = sender
+        smtpMailData.to = [{
+            email: email,
+        }];
+        smtpMailData.subject = 'Reset Password!';
+        smtpMailData.htmlContent = `<html><body><p>Reset Passowrd</p></body></html>`;
+        const res = await apiInstance.sendTransacEmail(smtpMailData)
+        console.log(res)
+
+
+    } catch (err) {
+        console.log('ERROR IS>>>>>', err)
+    }
+
 }
